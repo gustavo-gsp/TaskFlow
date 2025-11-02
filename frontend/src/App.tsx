a448bd6bd4e541d082f5329e3af7bccd9bdf7264
gustavo-gsp <gustavo.s.pinho@hotmail.com>
2025-11-02 19:31:02 -0300

    feat: estrutura inicial do TaskFlow

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center text-2xl font-bold">
+      TaskFlow 🚀
+    </div>
+  );
+}
---
+++ frontend/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const qc = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={qc}>
+    <App />
+  </QueryClientProvider>
+);
